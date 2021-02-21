f6b59056061854bdcf9b0b10cb5d026fb722fd40
swauthie <samuel.wauthier@ugent.be>
2021-02-21 20:57:47 +0100

    normal rainbow

--- main.py
+++ main.py
@@ -150,7 +150,7 @@ else:
     if T % args.replay_frequency == 0:
       dqn.reset_noise()  # Draw a new set of noisy weights
 
-    action = dqn.act_e_greedy(state, epsilon=0.001)  # Choose an action greedily (with noisy weights)
+    action = dqn.act(state)  # Choose an action greedily (with noisy weights)
     next_state, reward, done = env.step(action)  # Step
     if args.reward_clip > 0:
       reward = max(min(reward, args.reward_clip), -args.reward_clip)  # Clip rewards
